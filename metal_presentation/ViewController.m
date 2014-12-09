843db03d06383a63afb2aa12594696afd025affe
Nate <nater.hat@gmail.com>
2014-12-08 18:00:40 -0800

    add the variables to continue the setup of metal.

--- metal_presentation/ViewController.m
+++ metal_presentation/ViewController.m
@@ -8,9 +8,28 @@
 
 #import "ViewController.h"
 
+@import Metal;
 @import QuartzCore.CADisplayLink;
+@import QuartzCore.CAMetalLayer;
 
 @interface ViewController ()
+{
+    // setup
+    CAMetalLayer *_layer;
+    id<MTLDevice> _device;
+    id<MTLLibrary> _library;
+    id<MTLCommandQueue> _commandQueue;
+    id<MTLRenderPipelineState> _renderPipeline;
+    id<MTLDepthStencilState> _depthState;
+    id<MTLTexture> _depthTexture;
+    id<MTLBuffer> _vertexBuffer;
+    id<MTLBuffer> _indexBuffer;    
+    
+    // per frame
+    id<MTLCommandBuffer> _commandBuffer;
+    id<MTLCommandEncoder> _commandEncoder;
+    id<MTLDrawable> _drawable;
+}
 @property (nonatomic) CADisplayLink *ticker;
 @end
 
@@ -18,12 +37,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
-    _ticker = [CADisplayLink displayLinkWithTarget:self selector:@selector(update)];
+    
+    _ticker = [CADisplayLink displayLinkWithTarget:self selector:@selector(perFrame)];
 }
 
-- (void)update
+- (void)setup
+{
+    
+}
+
+- (void)beginRenderPass
+{
+    
+}
+
+- (void)setupRenderPipeline
+{
+    
+}
+
+- (void)draw
+{
+    
+}
+
+- (void)endRenderPass
+{
+    
+}
+
+- (void)commit
+{
+    
+}
+
+- (void)perFrame
 {
     
 }
