99c4ddc54d83c83c79d99c4742b0547cd7731eb3
Nate <nater.hat@gmail.com>
2014-12-08 19:20:37 -0800

    setting things up

--- metal_presentation/ViewController.m
+++ metal_presentation/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "NTShared.h"
 
 @import Metal;
 @import QuartzCore.CADisplayLink;
@@ -15,20 +16,21 @@
 @interface ViewController ()
 {
     // setup
-    CAMetalLayer *_layer;
+    CAMetalLayer *_metalLayer;
     id<MTLDevice> _device;
     id<MTLLibrary> _library;
     id<MTLCommandQueue> _commandQueue;
     id<MTLRenderPipelineState> _renderPipeline;
     id<MTLDepthStencilState> _depthState;
-    id<MTLTexture> _depthTexture;
+    id<MTLTexture> _depthTexture; // will be initialize in render pass setup
     id<MTLBuffer> _vertexBuffer;
     id<MTLBuffer> _indexBuffer;
+    id<MTLBuffer> _uniformBuffer;
     
     // per frame
     id<MTLCommandBuffer> _commandBuffer;
     id<MTLCommandEncoder> _commandEncoder;
-    id<MTLDrawable> _drawable;
+    id<CAMetalDrawable> _drawable;
 }
 @property (nonatomic) CADisplayLink *ticker;
 @end
@@ -46,32 +48,78 @@
 
 - (void)setup
 {
+    _device = MTLCreateSystemDefaultDevice();
     
+    _metalLayer = (id)self.view.layer;
+    [_metalLayer setDevice:_device];
+    
+    _commandQueue = [_device newCommandQueue];
+    
+    _library = [_device newDefaultLibrary];
+    
+    // shader functions
+    id<MTLFunction> vertexFunction = [_library newFunctionWithName:@"colorVertex"];
+    id<MTLFunction> fragmentFunction = [_library newFunctionWithName:@"colorFragment"];
+    
+    // set render pipeline
+    MTLRenderPipelineDescriptor *renderPipelineDescriptor = [MTLRenderPipelineDescriptor new];
+    renderPipelineDescriptor.depthAttachmentPixelFormat = MTLPixelFormatDepth32Float;
+    renderPipelineDescriptor.colorAttachments[0].pixelFormat = MTLPixelFormatRGBA8Unorm;
+    [renderPipelineDescriptor setVertexFunction:vertexFunction];
+    [renderPipelineDescriptor setFragmentFunction:fragmentFunction];
+    
+    NSError *error = nil;
+    _renderPipeline = [_device newRenderPipelineStateWithDescriptor:renderPipelineDescriptor error:&error];
+    if (error) {
+        NSLog(@"%@", error.localizedDescription);
+    }
+    
+    // set depth state
+    MTLDepthStencilDescriptor *depthDescriptor = [MTLDepthStencilDescriptor new];
+    [depthDescriptor setDepthCompareFunction:MTLCompareFunctionLess];
+    [depthDescriptor setDepthWriteEnabled:YES];
+    
+    _depthState = [_device newDepthStencilStateWithDescriptor:depthDescriptor];
+    
+    // set buffers
 }
 
 - (void)beginRenderPass
 {
+    // create command buffer
+    
+    // get drawable texture
+    
+    // create command encoder
     
 }
 
 - (void)setupRenderPipeline
 {
+    // set render pipeline
+    
+    // set depth
+    
+    // set buffers
+    
+    // set textures
+    // . . . there are none at this moment
     
 }
 
 - (void)draw
 {
-    
+    // draw
 }
 
 - (void)endRenderPass
 {
-    
+    // end encoder
 }
 
 - (void)commit
 {
-    
+    // commit buffer
 }
 
 - (void)perFrame
